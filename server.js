ea7c6f0a379b2cbb32e885ee1f2711df757e7048
Chris Chia <chrischia2002@gmail.com>
2025-04-03 09:02:57 +0800

    7.1.8

--- server.js
+++ server.js
@@ -135,8 +135,8 @@ app.post("/pay", (req, res) => {
             payment_method: "paypal"
         },
         redirect_urls: {
-            return_url: "http://localhost:3000/success",
-            cancel_url: "http://localhost:3000/cancel"
+            return_url: "http://${process.env.BASE_URL}/success",
+            cancel_url: "http://${process.env.BASE_URL}/cancel"
         },
         transactions: [{
             amount: {
