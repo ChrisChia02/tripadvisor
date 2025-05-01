92eb029a70cbc82a6f42cf5cd6732aed6906382f
Chris Chia <chrischia2002@gmail.com>
2025-05-02 04:15:06 +0800

    8.1.12

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ async function sendWhatsAppMessage(date) {
     type: "template",
     template: {
       name: "tripadvisor", // Ensure this matches your template name
-      language: { code: "en_US" },
+      language: { code: "en" },
       components: [
         {
           type: "body",
